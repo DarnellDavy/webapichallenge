59062b54a83391e746842f296736f6d5a5e25a3e
Darnell Davy <darnelldavy@darnells-mbp.myfiosgateway.com>
2021-10-07 19:42:06 -0400

    set up project

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+var questionsArray = [
+{question:"What is syntax to make a new directory on Terminal?", choices: ["cd","mkdir", "git branch", "git commit"],
+answer: "mkdir"
+},
+
+{question:"What does md stand for?", choices: ["cd","mkdir", "git branch", "git commit"],
+answer: "mkdir"
+},
+
+{question:"What is syntax to make a new directory on Terminal?", choices: ["cd","mkdir", "git branch", "git commit"],
+answer: "mkdir"
+},
+
+{question:"What is syntax to make a new directory on Terminal?", choices: ["cd","mkdir", "git branch", "git commit"],
+answer: "mkdir"
+},
+
+{question:"What is syntax to make a new directory on Terminal?", choices: ["cd","mkdir", "git branch", "git commit"],
+answer: "mkdir"
+},
+
+]
+
+console.log(questionsArray[1].question)
